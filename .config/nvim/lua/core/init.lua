a9d13d23a0b0c6f3461c3d1e3ba6a4dfb1cd39f0
Brett Henderson <BrettsVoid@gmail.com>
2024-12-26 18:02:06 +0000

    remove .config directory

--- .config/nvim/lua/core/init.lua
+++
@@ -1,4 +0,0 @@
-require 'core.keymaps'
-require 'core.diagnostics'
-require 'core.options'
-require 'core.helpers'
--- .config/nvim/lua/plugins/diffview.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/sindrets/diffview.nvim
-return {
-  'sindrets/diffview.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  opts = {},
-}

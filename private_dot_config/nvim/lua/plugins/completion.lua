a0f05fc7c6d457714d447ef37e6cf0fb39a29439
Brett Henderson <BrettsVoid@gmail.com>
2024-12-27 17:27:20 +0000

    nvim: add vim-dadbod

--- private_dot_config/nvim/lua/plugins/completion.lua
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -1,129 +1,133 @@
 return {
-  -- https://github.com/Saghen/blink.cmp
-  {
-    'saghen/blink.cmp',
-    --enabled = false,
-    dependencies = {
-      -- optional: provides snippets for the snippet source
-      'rafamadriz/friendly-snippets',
-      'onsails/lspkind.nvim',
-    },
-    version = '*',
+	-- https://github.com/Saghen/blink.cmp
+	{
+		"saghen/blink.cmp",
+		--enabled = false,
+		dependencies = {
+			-- optional: provides snippets for the snippet source
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind.nvim",
+		},
+		version = "*",
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release
-        use_nvim_cmp_as_default = false,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono',
-      },
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			appearance = {
+				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+				-- Useful for when your theme doesn't support blink.cmp
+				-- will be removed in a future release
+				use_nvim_cmp_as_default = false,
+				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
 
-      completion = {
-        accept = { auto_brackets = { enabled = true } },
+			completion = {
+				accept = { auto_brackets = { enabled = true } },
 
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 250,
-          treesitter_highlighting = true,
-          window = { border = 'rounded' },
-        },
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 250,
+					treesitter_highlighting = true,
+					window = { border = "rounded" },
+				},
 
-        list = {
-          selection = function(ctx)
-            return ctx.mode == 'cmdline' and 'auto_insert' or 'preselect'
-          end,
-        },
+				list = {
+					selection = function(ctx)
+						return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+					end,
+				},
 
-        menu = {
-          border = 'rounded',
-          cmdline_position = function()
-            if vim.g.ui_cmdline_pos ~= nil then
-              local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
-              return { pos[1] - 1, pos[2] }
-            end
-            local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
-            return { vim.o.lines - height, 0 }
-          end,
-          draw = {
-            columns = {
-              { 'kind_icon', 'label', gap = 1 },
-              { 'kind' },
-            },
-            components = {
-              kind_icon = {
-                text = function(item)
-                  local kind = require('lspkind').symbol_map[item.kind] or ''
-                  return kind .. ' '
-                end,
-                highlight = 'CmpItemKind',
-              },
-              label = {
-                text = function(item)
-                  return item.label
-                end,
-                highlight = 'CmpItemAbbr',
-              },
-              kind = {
-                text = function(item)
-                  return item.kind
-                end,
-                highlight = 'CmpItemKind',
-              },
-            },
-          },
-        },
-      },
+				menu = {
+					border = "rounded",
+					cmdline_position = function()
+						if vim.g.ui_cmdline_pos ~= nil then
+							local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+							return { pos[1] - 1, pos[2] }
+						end
+						local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+						return { vim.o.lines - height, 0 }
+					end,
+					draw = {
+						columns = {
+							{ "kind_icon", "label", gap = 1 },
+							{ "kind" },
+						},
+						components = {
+							kind_icon = {
+								text = function(item)
+									local kind = require("lspkind").symbol_map[item.kind] or ""
+									return kind .. " "
+								end,
+								highlight = "CmpItemKind",
+							},
+							label = {
+								text = function(item)
+									return item.label
+								end,
+								highlight = "CmpItemAbbr",
+							},
+							kind = {
+								text = function(item)
+									return item.kind
+								end,
+								highlight = "CmpItemKind",
+							},
+						},
+					},
+				},
+			},
 
-      keymap = {
-        preset = 'default',
-        --
-      },
+			keymap = {
+				preset = "default",
+				--
+			},
 
-      -- Experimental signature help support
-      signature = {
-        enabled = true,
-        window = { border = 'rounded' },
-      },
+			-- Experimental signature help support
+			signature = {
+				enabled = true,
+				window = { border = "rounded" },
+			},
 
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-        cmdline = function()
-          local type = vim.fn.getcmdtype()
-          -- Search forward and backward
-          if type == '/' or type == '?' then
-            return { 'buffer' }
-          end
-          -- Commands
-          if type == ':' then
-            return { 'cmdline' }
-          end
-          return {}
-        end,
-        providers = {
-          lsp = {
-            min_keyword_length = 1, -- Number of characters to trigger provider
-            score_offset = 0, -- Boost/penalize the score of the items
-          },
-          path = {
-            min_keyword_length = 0,
-          },
-          snippets = {
-            min_keyword_length = 2,
-          },
-          buffer = {
-            min_keyword_length = 5,
-            max_items = 5,
-          },
-        },
-      },
-    },
-    -- allows extending the providers array elsewhere in your config
-    -- without having to redefine it
-    --opts_extend = { 'sources.default' },
-  },
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "dadbod" },
+				cmdline = function()
+					local type = vim.fn.getcmdtype()
+					-- Search forward and backward
+					if type == "/" or type == "?" then
+						return { "buffer" }
+					end
+					-- Commands
+					if type == ":" then
+						return { "cmdline" }
+					end
+					return {}
+				end,
+				providers = {
+					lsp = {
+						min_keyword_length = 1, -- Number of characters to trigger provider
+						score_offset = 0, -- Boost/penalize the score of the items
+					},
+					path = {
+						min_keyword_length = 0,
+					},
+					snippets = {
+						min_keyword_length = 2,
+					},
+					buffer = {
+						min_keyword_length = 5,
+						max_items = 5,
+					},
+					dadbod = {
+						name = "Dadbod",
+						module = "vim_dadbod_completion.blink",
+					},
+				},
+			},
+		},
+		-- allows extending the providers array elsewhere in your config
+		-- without having to redefine it
+		opts_extend = { "sources.default" },
+	},
 }

e347d45c8e9212ae922ab2891cc54fb4dbab28c8
Brett Henderson <BrettH@m2north.com>
2025-06-19 08:55:50 +0100

    update yabai config to support unmanaged floating windows

--- private_dot_config/yabai/yabai_env.sh
+++ private_dot_config/yabai/yabai_env.sh
@@ -3,25 +3,27 @@
 # To get the names of all the running applications
 # yabai -m query --windows | jq -r '.[].app'
 
-# apps_transparent="(Spotify|kitty|Neovide|Google Chrome|Code|WezTerm|Ghostty)"
-# apps_transparent="(Neovide|Code)"
+# apps_transparent="^(Spotify|kitty|Neovide|Google Chrome|Code|WezTerm|Ghostty)$"
+# apps_transparent="^(Neovide|Code)$"
 
 display_resolution=$(system_profiler SPDisplaysDataType | grep Resolution)
 if [[ $(echo "$display_resolution" | grep -c "Resolution") -ge 2 ]]; then
-  apps_stream="(Microsoft Edge|OBS Studio|Jitsi Meet|Discord)"
+  apps_stream="^(Microsoft Edge|OBS Studio|Jitsi Meet|Discord)$"
   # This keeps apps always below, seems to be working fine when I switch to other
   # apps
-  apps_mgoff_below="(System Settings|Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|Activity Monitor|App StoreSoftware Update|TestRig|Gemini|Raycast|OBS Studio|Microsoft Edge|Cisco Packet Tracer|Stickies|kitty|ProLevel|Photo Booth|Hand Mirror|SteerMouse|remote-viewer|Jitsi Meet|DaVinci Resolve|Discord)"
+  apps_mgoff_below="^(System Settings|Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|Activity Monitor|App StoreSoftware Update|TestRig|Gemini|Raycast|OBS Studio|Microsoft Edge|Cisco Packet Tracer|Stickies|kitty|ProLevel|Photo Booth|Hand Mirror|SteerMouse|remote-viewer|Jitsi Meet|DaVinci Resolve|Discord)$"
 else
-  apps_stream="()"
+  apps_stream="^()$"
   # This keeps apps always below, seems to be working fine when I switch to other
   # apps
-  apps_mgoff_below="(System Settings|Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|Activity Monitor|App StoreSoftware Update|TestRig|Gemini|Raycast|OBS Studio|Microsoft Edge|Cisco Packet Tracer|Stickies|kitty|ProLevel|Photo Booth|Hand Mirror|SteerMouse|remote-viewer|Jitsi Meet|DaVinci Resolve)"
+  #apps_mgoff_below="^(System Settings|Calculator|iStat Menus|Hammerspoon|BetterDisplay|GIMP|Notes|Activity Monitor|App Store|Software Update|TestRig|Gemini|Raycast|OBS Studio|Microsoft Edge|Cisco Packet Tracer|Stickies|kitty|ProLevel|Photo Booth|Hand Mirror|SteerMouse|remote-viewer|Jitsi Meet|DaVinci Resolve)$"
+  apps_mgoff_below="^(System Settings|Calculator|Hammerspoon|Notes|Activity Monitor|App Store|Software Update|Gemini|Raycast|Photo Booth|Jitsi Meet)$"
+  
 fi
 
 # Apps that I want to always show, even when I have a transparent app focused
-apps_transp_ignore="(kitty)"
-apps_scratchpad="(Udemy|WezTerm)"
+apps_transp_ignore="^(kitty)$"
+apps_scratchpad="^(Udemy|WezTerm)$"
 # apps_transp_ignore="(kitty|CleanShot X)"
 
 # Apps excluded from window management, so you can resize them and move them around
@@ -29,7 +31,7 @@ apps_scratchpad="(Udemy|WezTerm)"
 
 # I had to move them away from normal, because all these apps would stay on top
 # of other apps
-# apps_mgoff_normal="()"
+apps_mgoff_normal="^(DBeaver Community)$"
 
 # This keeps apps always on the top
-apps_mgoff_above="()"
+apps_mgoff_above="^()$"

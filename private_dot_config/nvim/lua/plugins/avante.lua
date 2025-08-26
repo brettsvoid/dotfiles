38a2e9f914f2717f01a678cb78d2f8fb50d76578
Brett Henderson <BrettH@m2north.com>
2025-08-26 17:32:12 +0100

    update avante.nvim plugin

--- private_dot_config/nvim/lua/plugins/avante.lua
+++ private_dot_config/nvim/lua/plugins/avante.lua
@@ -1,29 +1,26 @@
 -- https://github.com/yetone/avante.nvim
 return {
 	"yetone/avante.nvim",
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	-- ⚠️ must add this setting! ! !
+	build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+		or "make",
 	event = "VeryLazy",
 	version = false, -- set this if you want to always pull the latest change
+	---@module 'avante'
+	---@type avante.Config
 	opts = {
 		---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
 		---@type Provider
 		provider = "claude",
-		auto_suggestions_provider = "claude",
-		-- cursor_applying_provider = "openai",
-		-- behaviour = {
-		-- 	enable_cursor_planning_mode = true, -- enable cursor planning mode
-		-- },
-		-- web_search_engine = {
-		-- 	provider = "tavily",
-		-- 	proxy = nil,
-		-- },
-		--		vendors = {
 		providers = {
 			claude = {
 				endpoint = "https://api.anthropic.com",
-				model = "claude-3-5-sonnet-20241022",
+				model = "claude-sonnet-4-20250514",
+				timeout = 30000, -- Timeout in milliseconds
 				extra_request_body = {
 					temperature = 0.75,
-					max_tokens = 4096,
+					max_tokens = 20480,
 				},
 			},
 			openai = {
@@ -32,7 +29,7 @@ return {
 				extra_request_body = {
 					temperature = 0,
 					max_tokens = 32768,
-				}
+				},
 				--reasoning_effort = "medium"
 			},
 			copilot = {
@@ -43,30 +40,28 @@ return {
 				extra_request_body = {
 					temperature = 0,
 					max_tokens = 4096,
-				}
+				},
 			},
-		-- groq = { -- define groq provider
-		--     __inherited_from = 'openai',
-		--     api_key_name = 'GROQ_API_KEY',
-		--     endpoint = 'https://api.groq.com/openai/v1/',
-		--     model = 'llama-3.3-70b-versatile',
-		--     max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
-		-- },
-		--		},
+			-- groq = { -- define groq provider
+			--     __inherited_from = 'openai',
+			--     api_key_name = 'GROQ_API_KEY',
+			--     endpoint = 'https://api.groq.com/openai/v1/',
+			--     model = 'llama-3.3-70b-versatile',
+			--     max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
+			-- },
+			--		},
 		},
 	},
-
-	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-	build = "make",
-	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 		--- The below dependencies are optional,
 		--"echasnovski/mini.pick", -- for file_selector provider mini.pick
-		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		--"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		"ibhagwan/fzf-lua", -- for file_selector provider fzf
+		"stevearc/dressing.nvim", -- for input provider dressing
+		"folke/snacks.nvim", -- for input provider snacks
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 		"zbirenbaum/copilot.lua", -- for providers='copilot'
 		{
 			-- support for image pasting
@@ -93,23 +88,5 @@ return {
 			},
 			ft = { "markdown", "Avante" },
 		},
-		-- Might be outdated
-		-- {
-		-- 	"MeanderingProgrammer/render-markdown.nvim",
-		-- 	optional = true,
-		-- 	config = function(_, opts)
-		-- 		local ok, render_markdown = pcall(require, "render-markdown")
-		-- 		if ok then
-		-- 			-- If plugin exists, extend its configuration
-		-- 			opts.file_types = vim.list_extend(opts.file_types or {}, { "Avante" })
-		-- 			render_markdown.setup(opts)
-		-- 		else
-		-- 			-- If plugin doesn't exist, set up new configuration
-		-- 			require("render-markdown").setup({
-		-- 				file_types = { "markdown", "Avante" },
-		-- 			})
-		-- 		end
-		-- 	end,
-		-- },
 	},
 }

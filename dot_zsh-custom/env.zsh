8fe7ed1b63f32339ca371df266169fe3942ea656
Brett Henderson <BrettH@m2north.com>
2026-01-23 12:18:15 +0000

    fix zsh init order for bws command availability

--- dot_zsh-custom/env.zsh
+++ dot_zsh-custom/env.zsh
@@ -2,16 +2,16 @@
 
 # Editor
 if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='vim'
+  export EDITOR="vim"
 else
-  export EDITOR='nvim'
+  export EDITOR="nvim"
 fi
 
 # React editor for stack traces
-export REACT_EDITOR=nvim
+export REACT_EDITOR="nvim"
 
 # Android SDK
-export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_HOME="$HOME/Library/Android/sdk"
 
 # pnpm
 export PNPM_HOME="/Users/brett/Library/pnpm"
@@ -20,4 +20,5 @@ export PNPM_HOME="/Users/brett/Library/pnpm"
 export CONDA_BASE="/opt/homebrew/anaconda3"
 
 # Claude Sentry MCP
-export SENTRY_ACCESS_TOKEN_CLAUDE="$(bws secret get 59d40097-f5d2-4fd3-bc0d-b3d400f0a9f8 --access-token $BWS_ACCESS_TOKEN | jq -r '.value')"
+export SENTRY_ACCESS_TOKEN_CLAUDE="$(bws secret get 59d40097-f5d2-4fd3-bc0d-b3d400f0a9f8 --access-token $BWS_ACCESS_TOKEN 2>/dev/null | jq -r '.value')"
+export SENTRY_ACCESS_TOKEN_MCP="$SENTRY_ACCESS_TOKEN_CLAUDE"

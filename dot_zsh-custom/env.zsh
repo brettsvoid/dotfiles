ae65ff958382b19ae3109bbe0f4478665a6b06d2
Brett Henderson <BrettsVoid@gmail.com>
2026-04-27 09:03:17 +0100

    zsh(env): add nomad variables

--- dot_zsh-custom/env.zsh
+++ dot_zsh-custom/env.zsh
@@ -19,6 +19,11 @@ export PNPM_HOME="/Users/brett/Library/pnpm"
 # Conda
 export CONDA_BASE="/opt/homebrew/anaconda3"
 
+# PI Cluster
+export NOMAD_ADDR="http://192.168.1.50:4646"
+export NOMAD_OPERATOR_TOKEN="$(bws secret get f4ce7100-6725-470b-9a55-b43801659f7f --access-token $BWS_ACCESS_TOKEN 2>/dev/null | jq -r '.value')"
+export NOMAD_TOKEN="$NOMAD_OPERATOR_TOKEN"
+
 # Claude Sentry MCP
 export SENTRY_ACCESS_TOKEN_CLAUDE="$(bws secret get 59d40097-f5d2-4fd3-bc0d-b3d400f0a9f8 --access-token $BWS_ACCESS_TOKEN 2>/dev/null | jq -r '.value')"
 export SENTRY_ACCESS_TOKEN_MCP="$SENTRY_ACCESS_TOKEN_CLAUDE"
